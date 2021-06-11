4ed70e0cd847057e5e160de0ee66abba9f45ac78
Lanre <laniyan@hotmail.co.uk>
2021-06-11 02:10:21 +0100

    Update EF Database Name
    Update the EF database name to be EFData

--- RMDataManager/Models/IdentityModels.cs
+++ RMDataManager/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace RMDataManager.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("ETData", throwIfV1Schema: false)
         {
         }
         
